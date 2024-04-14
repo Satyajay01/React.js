1570280f111f1adc84c44f825614af5b0fcdd3d4
satyajay01 <satyajaydibya0@gmail.com>
2024-04-14 22:21:43 +0530

    Nested list with Nested Array, Object

---
+++ (18)_React_Nested_list_with_Nested_Array/src/App.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const App = () => {
+  const users = [
+    {
+      Name: "Jay",
+      Email: "jay@gmail.com",
+      Address: [
+        { H_no: "10", City: "Raipur", Country: "india" },
+        { H_no: "11", City: "Bilaspur", Country: "india" },
+        { H_no: "12", City: "Janjgir Champa", Country: "india" },
+        { H_no: "13", City: "Raigarh", Country: "india" },
+      ],
+    },
+    {
+      Name: "Satyajay",
+      Email: "Satyajay@gmail.com",
+      Address: [
+        { H_no: "10", City: "Raipur", Country: "india" },
+        { H_no: "11", City: "Bilaspur", Country: "india" },
+        { H_no: "12", City: "Janjgir Champa", Country: "india" },
+        { H_no: "13", City: "Raigarh", Country: "india" },
+      ],
+    },
+    {
+      Name: "Abhi",
+      Email: "Abhi@gmail.com",
+      Address: [
+        { H_no: "10", City: "Raipur", Country: "india" },
+        { H_no: "11", City: "Bilaspur", Country: "india" },
+        { H_no: "12", City: "Janjgir Champa", Country: "india" },
+        { H_no: "13", City: "Raigarh", Country: "india" },
+      ],
+    },
+    {
+      Name: "Satya",
+      Email: "Satya@gmail.com",
+      Address: [
+        { H_no: "10", City: "Raipur", Country: "india" },
+        { H_no: "11", City: "Bilaspur", Country: "india" },
+        { H_no: "12", City: "Janjgir Champa", Country: "india" },
+        { H_no: "13", City: "Raigarh", Country: "india" },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <h3>List with Nested Array</h3>
+      <table border={1}>
+
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Address</td>
+          </tr>
+    
+        {users.map((item) => (
+          <tr>
+            <td>{item.Name}</td>
+            <td>{item.Email}</td>
+            <td>
+              {item.Address.map((data) => (
+                <tr>
+                  <td>H_no: {data.H_no}</td>
+                  <td>City: {data.City}</td>
+                  <td>Country: {data.Country}</td>
+                </tr>
+              ))}
+            </td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+export default App;
