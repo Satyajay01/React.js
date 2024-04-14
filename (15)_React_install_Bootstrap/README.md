58177c26db15d04cd0d43b739412259e46a08547
satyajay01 <satyajaydibya0@gmail.com>
2024-04-14 20:46:26 +0530

    install Bootstrap

---
+++ (15)_React_install_Bootstrap/README.md
@@ -0,0 +1,14 @@
+# Bootstrap
+https://getbootstrap.com
+
+
+(Step 1)Terminal = npm i bootstrap
+
+(Step 2) install bootstrap best extension = (Name: IntelliSense for CSS class names in HTML
+Link = https://marketplace.visualstudio.com/items?itemName=Zignd.html-css-class-completion)
+
+(Step 3) Open App.jsx and import =
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
---
+++ (15)_React_install_Bootstrap/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-danger">Bootstrap</h1>
+            <div className="bg-info p-5">
+                <p className="">
+                    This is example for using button with
+                    bootstrap styling
+                </p>
+                <a
+                    className="btn btn-primary"
+                    data-bs-toggle="collapse"
+                    href="#collapseExample"
+                    role="button"
+                    aria-expanded="false"
+                    aria-controls="collapseExample"
+                >
+                    Bootstrap button
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default App;
